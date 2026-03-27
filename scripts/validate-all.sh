6f0a18aef9caa660b5ed24918020b9c7b9304dc9
Lars Kellogg-Stedman <lars@oddbit.com>
2026-03-27 09:35:00 -0400

    Add schema for town information

--- scripts/validate-all.sh
+++ scripts/validate-all.sh
@@ -8,5 +8,8 @@ node scripts/validate-markdown.js
 echo "=== validate races.yaml ==="
 node scripts/validate-races.js
 
+echo "=== validate towns.yaml ==="
+node scripts/validate-data.js schemas/towns.yaml content/_data/towns.yaml
+
 echo "=== validate javascript code ==="
 npx biome ci
